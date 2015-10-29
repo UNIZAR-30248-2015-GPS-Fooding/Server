d4f77eefe66d0ed39e61fff03b22b3f893a8d92e
Alejandro <donrondon40@gmail.com>
2015-10-29 11:51:47 +0100

    Script init.bat corregido

--- Scripts_git/Windows/init.bat
+++ Scripts_git/Windows/init.bat
@@ -1,5 +1,9 @@
 @echo off
 
+rem Directorio del proyecto de git (donde esta la carpeta .git)
+set /P dir="Enter git directory: "
+cd /d "%dir%"
+
 rem github repo url
 set /P remote="Enter url of github repository: "
 

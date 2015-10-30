c2069f2324eb7776d713460f5a37b55d03ffeb55
Patricia Lazaro <plazaro1994@gmail.com>
2015-10-30 14:53:59 +0100

    Probando tests

---
+++ src/main/java/test/DbMethodsTest.java
@@ -0,0 +1,54 @@
+/*
+ * DbMethodsTest.java v1.0 30/10/2015
+ */
+
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import data.Receta;
+import db.DbMethods;
+
+public class DbMethodsTest {
+	/**
+	 * Clase para tests sobre los metodos de acceso
+	 * 
+	 * @version 1.0 - Test de lista de ingredientes - Test de lista de recetas
+	 * @date 23/10/2015
+	 */
+
+	/**
+	 * Test para comprobar que el metodo para obtener la lista de ingredientes
+	 * no devuelve <null> o lista vacia
+	 */
+	@Test
+	public void test_lista_ingredientes() {
+		List<String> ings = DbMethods.get_lista_ingredientes();
+		assertTrue(ings != null && ings.size() > 0);
+	}
+
+	/**
+	 * Test para comprobar que el metodo para obtener recetas (de forma
+	 * generica, sin filtrados) no devuelve <null> o lista vacia
+	 */
+	@Test
+	public void test_recetas() {
+		List<Receta> recetas = DbMethods.get_recetas(null, null, null);
+		assertTrue(recetas != null && recetas.size() > 0);
+	}
+
+	/**
+	 * Test para comprobar que el metodo para obtener recetas por nombre no
+	 * devuelve <null> o lista vacia
+	 */
+	@Test
+	public void test_recetas_nombre() {
+		List<Receta> recetas = DbMethods.get_recetas("mac", null, null);
+		assertTrue(recetas != null && recetas.size() > 0);
+	}
+
+}

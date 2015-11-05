0de9781dc607ec23797da3610ee7db5da8c0ee78
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-05 18:00:29 +0100

    code coverage (5)

--- Scripts_git/Windows/push.bat
+++ Scripts_git/Windows/push.bat
@@ -19,6 +19,5 @@ rem Push to Github
 git push github %branch%
 
 rem Get code coverage
-scp 5624a20f0c1e669609000099@fooding-gpsfooding.rhcloud.com:./app-root/repo/target/site/jacoco/index.html ./Reports/
-
+"%SYSTEMDRIVE%\Program Files\Git\bin\bash.exe"" --login -i -c "scp 5624a20f0c1e669609000099@fooding-gpsfooding.rhcloud.com:./app-root/repo/target/site/jacoco/index.html ./Reports/"
 pause

136c0e0a8cfde64a43d61796833fdd8a29edcbcd
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-05 18:40:39 +0100

    terminado

--- Scripts_git/Windows/push.bat
+++ Scripts_git/Windows/push.bat
@@ -20,7 +20,5 @@ git push github %branch%
 
 rem Get code coverage
 set "date=%DATE%%TIME%"
-"%SYSTEMDRIVE%\Program Files\Git\bin\bash.exe" --login -i -c "scp 5624a20f0c1e669609000099@fooding-gpsfooding.rhcloud.com:./app-root/repo/target/site/jacoco/index.html ./Reports/date.html"
-cd "Reports"
-ren "date.html" "%date%.html"
+"%SYSTEMDRIVE%\Program Files\Git\bin\bash.exe" --login -i -c "scp 5624a20f0c1e669609000099@fooding-gpsfooding.rhcloud.com:./app-root/repo/target/site/jacoco/index.html ./Reports/index.html"
 pause

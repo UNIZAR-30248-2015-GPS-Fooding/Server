2e6459a6a88381da04260ccc2ad978408a892d96
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-05 18:07:13 +0100

    code coverage enhancement

--- Scripts_git/Windows/push.bat
+++ Scripts_git/Windows/push.bat
@@ -19,5 +19,6 @@ rem Push to Github
 git push github %branch%
 
 rem Get code coverage
-"%SYSTEMDRIVE%\Program Files\Git\bin\bash.exe" --login -i -c "scp 5624a20f0c1e669609000099@fooding-gpsfooding.rhcloud.com:./app-root/repo/target/site/jacoco/index.html ./Reports/"
+set date=%DATE%%TIME%
+"%SYSTEMDRIVE%\Program Files\Git\bin\bash.exe" --login -i -c "scp 5624a20f0c1e669609000099@fooding-gpsfooding.rhcloud.com:./app-root/repo/target/site/jacoco/index.html ./Reports/date"
 pause

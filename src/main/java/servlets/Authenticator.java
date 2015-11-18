89cd15a571e4479a7a8d654c887931b58c753c35
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-18 15:08:13 +0100

    authneticator esqueleto

---
+++ src/main/java/servlets/Authenticator.java
@@ -0,0 +1,79 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Authenticator extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4656698343123568633L;
+
+	/**
+	 * Metodo para recibir las peticiones GET de la aplicacion
+	 * 
+	 * @version 1.0
+	 */
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String hash = req.getParameter("key");
+		manageKey(resp, hash);
+	}
+
+	/**
+	 * Metodo para recibir las peticiones POST de la aplicacion
+	 * 
+	 * @version 1.0
+	 */
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String hash = req.getParameter("key");
+		manageKey(resp, hash);
+	}
+	
+	/**
+	 * Metodo para verificar al usuario a partir de su @param key.
+	 * Escribe en @param resp el resultado de la verificacion
+	 * @throws IOException 
+	 */
+	private void manageKey (HttpServletResponse resp, String key) throws IOException{
+		PrintWriter out = resp.getWriter();
+		
+		boolean exito = false;	// true si se ha verificado al user
+		String mail = null;
+		
+		out.println("<!DOCTYPE html>");
+		if(exito){
+			out.println("<html>");
+			
+			out.println("<head>");
+			out.println("<title>Exito al verificar</title>");
+			out.println("</head>");
+			
+			out.println("<body>");
+			out.println("<p>Se ha verificado al usuario " + mail + "</p>");
+			out.println("</body>");
+			out.println("</html>");
+		}
+		else{
+			out.println("<html>");
+			
+			out.println("<head>");
+			out.println("<title>Fallo al verificar</title>");
+			out.println("</head>");
+			
+			out.println("<body>");
+			out.println("<p>No se ha podido verificar al usuario "
+					+ mail + "</p>");
+			out.println("</body>");
+			out.println("</html>");
+		}
+	}
+
+}
